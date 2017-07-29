fa1e86f16793f9508e662c9520b4fca0678cfb0e
nvarner <nathanmvarner@protonmail.com>
2017-07-29 09:20:51 -0400

    Writes to JSON file
    fit_data.py now uses settings and writes the step output to a JSON file.

--- src/io.py
+++ src/io.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
 
+
 def load_file(url):
     """Loads a file from URL. It can retrieve files locally and over HTTP."""
     request_type = url.split(":")[0]
@@ -16,6 +17,27 @@ def load_file(url):
 def load_json(url):
     """Loads JSON from a URL. See load_file for more info."""
     return json.loads(load_file(url))
+
+
+def write_file(url, contents):
+    """Writes to a file at a URL. It can only write to files locally."""
+    request_type = url.split(":")[0]
+
+    if request_type == "http":
+        raise ValueError("io.write_file can only write to local files, not files over http.")
+    else:
+        with open(url, 'w') as file:
+            file.writelines(contents)
+
+
+def make_json(obj, url, write_to_file=True):
+    """Writes JSON to a file. See write_file for more info. Can also return JSON string."""
+    json_string = json.dumps(obj)
+
+    if write_to_file:
+        write_file(url, json_string)
+    else:
+        return json_string
 
 
 def make_request(fitness_service, request, user_id="me"):
