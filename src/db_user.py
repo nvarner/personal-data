b09cbfef042497a3448429afd294fd2a7fde4bd5
nvarner <nathanmvarner@protonmail.com>
2017-07-29 13:01:44 -0400

    Translated DBUser.java to db_user.py
    The database user object is now in Python, and 19 lines shorter,too.

---
+++ src/db_user.py
@@ -0,0 +1,55 @@
+import hashlib
+import random
+import string
+
+
+class DBUser:
+    def __init__(self, username, password):
+        self.CODE_WORD_LEN = 50
+
+        self.username = username
+        self.password = password
+        self.codeword = None
+        self.random = random
+        self.string = string
+
+    def get_code_word(self):
+        if self.codeword is None:
+            unhashed_codeword = ""
+
+            # Populate the unhashed codeword randomly
+            for i in range(self.CODE_WORD_LEN):
+                unhashed_codeword.join(self.random.SystemRandom().choice(string.ascii_uppercase + string.digits))
+
+            m = hashlib.sha256()
+            m.update(bytes(unhashed_codeword))
+            self.codeword = m.digest()
+
+            return unhashed_codeword
+        else:
+            raise Exception("Codeword already generated! Store it the first time, and/or stop hacking. "
+                            "It's not very nice.")
+
+    def get_username(self, unhashed_codeword):
+        if self.codeword is None:
+            raise Exception("Codeword not generated! Generate the codeword and store it with get_code_word "
+                            "before using it.")
+        else:
+            m = hashlib.sha256()
+            m.update(bytes(unhashed_codeword))
+            if self.codeword == m.digest():
+                return self.username
+            else:
+                raise Exception("Bad codeword! Store it from getCodeword to use it.")
+
+    def get_password(self, unhashed_codeword):
+        if self.codeword is None:
+            raise Exception("Codeword not generated! Generate the codeword and store it with get_code_word " +
+                            "before using it.")
+        else:
+            m = hashlib.sha256()
+            m.update(bytes(unhashed_codeword))
+            if self.codeword == m.digest():
+                return self.password
+            else:
+                raise Exception("Bad codeword! Store it from getCodeword to use it.")
