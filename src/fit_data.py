7a5c11fe943da3f64beeb128e40aa9a42b22252a
medude <varner_nathan@yahoo.com>
2017-07-31 17:14:01 -0400

    Connect the two sides
    The database and Google Fit data collector are now connected! main.py automatically requests and stores yesterday's step count.

--- src/fit_data.py
+++ src/fit_data.py
@@ -6,34 +6,41 @@ from apiclient.discovery import build
 from src import auth
 from src import in_out
 
-if __name__ == "__main__":
-    settings = in_out.load_json("settings.json")
-    client_secrets_url = settings["client_secrets_url"]
-    oauth_scope = settings["oauth_scope"]
 
-    client_secrets = in_out.load_json(client_secrets_url)["installed"]
-    client_id = client_secrets["client_id"]
-    client_secret = client_secrets["client_secret"]
-    redirect_uri = client_secrets["redirect_uris"][0]
+class FitData:
+    def __init__(self, settings):
+        # Define settings (normally settings.json)
+        self.settings = settings
 
-    http, cred = auth.first_auth(client_id, client_secret, oauth_scope, redirect_uri)
+        # Variables for quick access to settings
+        client_secrets_url = self.settings["client_secrets_url"]
+        oauth_scope = self.settings["oauth_scope"]
 
-    http = auth.refresh_auth(cred)
+        # Authentication variables
+        client_secrets = in_out.load_json(client_secrets_url)["installed"]
+        self.client_id = client_secrets["client_id"]
+        self.client_secret = client_secrets["client_secret"]
+        self.redirect_uri = client_secrets["redirect_uris"][0]
 
-    fitness_service = build('fitness', 'v1', http=http)
+        self.http, self.cred = auth.first_auth(self.client_id, self.client_secret, oauth_scope, self.redirect_uri)
 
-    data_set = in_out.make_request(fitness_service, in_out.load_json("request.json"))
+    def get_data(self, request=in_out.load_json("request.json"), write_file=False, out_url="../../steps.json"):
+        self.http = auth.refresh_auth(self.cred)
 
-    steps_object = {}
+        fitness_service = build('fitness', 'v1', http=self.http)
+        data_set = in_out.make_request(fitness_service, request)
+        steps_object = {}
 
-    for bucket in data_set["bucket"]:
-        start_time = datetime.datetime.fromtimestamp(int(bucket["startTimeMillis"]) / 1000.0)
-        daily_steps = 0
+        for bucket in data_set["bucket"]:
+            start_time = datetime.datetime.fromtimestamp(int(bucket["startTimeMillis"]) / 1000.0)
+            daily_steps = 0
 
-        for dataset in bucket["dataset"]:
-            if len(dataset["point"]) is not 0:
-                daily_steps += dataset["point"][0]["value"][0]["intVal"]
+            for dataset in bucket["dataset"]:
+                if len(dataset["point"]) is not 0:
+                    daily_steps += dataset["point"][0]["value"][0]["intVal"]
 
-            steps_object[str(start_time)] = daily_steps
+                steps_object[str(start_time)] = daily_steps
 
-    in_out.make_json(steps_object, "../../steps.json")
+        if write_file:
+            in_out.make_json(steps_object, out_url)
+        return steps_object
--- src/main.py
+++ src/main.py
@@ -1,11 +1,33 @@
+from datetime import date, timedelta
+import time
+
 import in_out
 from db import DB
 from db_user import DBUser
+from fit_data import FitData
 
 if __name__ == "__main__":
-    login = in_out.load_json("../../login_data.json")
+    settings = in_out.load_json("settings.json")
+    login = in_out.load_json(settings["login_url"])
+    base_request = in_out.load_json(settings["base_request_url"])
 
     personal_info = DB("localhost", "personal_data", DBUser(login["username"], login["password"]))
-    personal_info.update("INSERT INTO health values ('2017/7/28', 4165);")
+    fit_data = FitData(settings)
+
+    yesterday = date.today() - timedelta(days=1)
+
+    start_request = int(time.mktime(time.strptime(yesterday.strftime("%Y-%m-%d") + " 00:00:00", '%Y-%m-%d %H:%M:%S'))) \
+        * 1000
+    end_request = int(time.mktime(time.strptime(yesterday.strftime("%Y-%m-%d") + " 23:59:59", '%Y-%m-%d %H:%M:%S'))) \
+        * 1000
+
+    request = base_request
+    request["startTimeMillis"] = start_request
+    request["endTimeMillis"] = end_request
+
+    yesterday_steps = fit_data.get_data(request=request)[yesterday.strftime("%Y-%m-%d") + " 00:00:00"]
+    print(yesterday_steps)
+    personal_info.update(
+        "INSERT INTO health values ('" + yesterday.strftime("%y/%m/%d") + "', " + str(yesterday_steps) + ");")
 
     # result = personal_info.query("SELECT * FROM `health` ORDER BY `steps` DESC")
